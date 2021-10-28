e6eb544abc9e67e4b3447a467e96f023e299653a
Mapashi <91593731+Mapashi@users.noreply.github.com>
2021-10-28 13:59:34 +0200

    Add files via upload

---
+++ vect2.py
@@ -0,0 +1,11 @@
+'''Funciones para rellenar un vector de 15 números enteros
+aleatorios'''
+import random
+list = []
+a = 0
+while(15 > a):
+    list.append(random.randint(1,10))
+    a = a + 1
+
+
+
